6cf2481a3d219c0474a24b1f134ea831eac1c72f
Marco Capucho <65176501+MarcoOliv@users.noreply.github.com>
2025-05-19 20:20:21 -0300

    Finalizado sistema de forma simples

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,11 +21,6 @@ function enviarParaWhatsapp(numero, mensagem) {
   window.open(url, '_blank'); // abre em nova aba
 }
 
-function adicionarAoCarrinho(nome){
-    carrinho.push(nome);
-    atualizarContador();
-}
-
 function atualizarContador(){
     const contador = document.getElementById('contador-carrinho');
     contador.textContent = carrinho.length;
@@ -33,4 +28,19 @@ function atualizarContador(){
 
 function abrirCarrinho(){
     alert("Itens no carrinho:\n" + carrinho.join('\n'));
+}
+
+function alternarCarrinho(nome) {
+  const index = carrinho.findIndex(item => item === nome);
+  const botao = document.getElementById(`botao-${nome.split(' ').join('')}`); // cria id sem espaços
+
+  if (index === -1) {
+    carrinho.push(nome);
+    botao.innerHTML = '<img src="assets/img/delete-svgrepo-com.png" style="filter: brightness(0) invert(1);">';
+  } else {
+    carrinho.splice(index, 1);
+    botao.innerHTML = '<img src="assets/img/svg_shoppingCart.png.png">';
+  }
+
+  atualizarContador();
 }
